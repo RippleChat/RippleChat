7089d7ebe79e57b34865f2bda549c90fa7b57d48
sebaschi <baschi99@windowslive.com>
2023-07-05 13:42:55 +0200

    Initial Commit

---
+++ RippleChat/RippleChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleChatApp.swift
+//  RippleChat
+//
+//  Created by Sebastian Lenzlinger on 05.07.23.
+//
+
+import SwiftUI
+
+@main
+struct RippleChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
